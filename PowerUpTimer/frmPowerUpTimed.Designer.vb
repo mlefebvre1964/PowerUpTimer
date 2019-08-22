5872fa563636a217520cb3d970eac704a80a959f
mlefebvre1964 <54377103+mlefebvre1964@users.noreply.github.com>
2019-08-22 08:52:50 -0400

    1stTime

---
+++ PowerUpTimer/frmPowerUpTimed.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPowerUpTimed
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.PowerUpTimer.My.Resources.Resources.Retina
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(35, 35)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(990, 750)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(445, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 46)
+        Me.Label1.TabIndex = 1
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(1060, 820)
+        Me.ShapeContainer1.TabIndex = 2
+        Me.ShapeContainer1.TabStop = False
+        '
+        'RectangleShape1
+        '
+        Me.RectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape1.BorderColor = System.Drawing.Color.Black
+        Me.RectangleShape1.BorderWidth = 10
+        Me.RectangleShape1.FillColor = System.Drawing.Color.Red
+        Me.RectangleShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.RectangleShape1.Location = New System.Drawing.Point(20, 20)
+        Me.RectangleShape1.Name = "RectangleShape1"
+        Me.RectangleShape1.Size = New System.Drawing.Size(1020, 780)
+        '
+        'frmPowerUpTimed
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1060, 820)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmPowerUpTimed"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Private WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Private WithEvents RectangleShape1 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+
+End Class
---
+++ PowerUpTimer/frmPowerUpTimed.vb
@@ -0,0 +1,110 @@
+﻿Public Class frmPowerUpTimed
+    'MRL 2019-08-07
+    Public cycles As Integer = 15
+    Public cyclecount As Integer = 0
+    Public cyclesleft As Integer = 0
+    Public jobPath As String = vbNullString
+    Public priority As Integer = 0
+
+    'This program displays a splash screen with a countdown timer
+    'After the specified timer duration, an executable will be started.
+    'The purpose of the program is to allow background applications to be up and running prior to 
+    'attempting to establishing connection with the UI
+    '
+    'The Power UP Run It.bat file contains the sequence to run the executable.
+    'An example entry in Power UP Run It.bat
+    ''START /LOW C:\780\PowerUpTimer.exe 45 C:\780\SPS780.exe 1
+    '/LOW runs the PowerUpTimer.exe in Low Priority
+    'The next entry is "C:\780\PowerUpTimer.exe" is the path to the PowerUpTimer.exe
+    'Following the path is the timer duration in the above example it is 45 seconds - if the entry is less than 5 the timer is set to 5 seconds if
+    'the duration is greater than 120 the timer is set to 120 seconds.
+    'Following the duration is the path to executable to run, here the path is "C:\780\SPS780.exe"
+    'Following duration is the priority in the above example the program will be run will RealTime priority
+    ' 0 = High (default)
+    ' 1 = Realtime 
+    ' 2 = Above Normal
+    ' anything else is regarded as Normal priority
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        Dim args() As String = Environment.GetCommandLineArgs()
+
+        'args(0) is the path to the PowerUpTimer.exe
+
+        If args.Length > 1 Then
+            cycles = Convert.ToInt32(args(1))
+
+            If cycles < 5 Then cycles = 5
+            If cycles > 120 Then cycles = 120
+
+        Else
+            cycles = 15
+
+        End If
+
+        If args.Length > 2 Then jobPath = args(2)
+
+        If args.Length > 3 Then priority = args(3)
+
+
+        ' Add any initialization after the InitializeComponent() call.
+        Label1.Text = "STARTING IN " & cycles.ToString & " SECONDS"
+        Timer1.Start()
+
+    End Sub
+    Public Sub New(ByVal duration As Integer)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        'cycles = duration
+        'Label1.Text = "STARTING IN " & cycles.ToString & " SECONDS"
+        'Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        cyclecount += 1
+        cyclesleft = cycles - cyclecount
+        Dim m As Integer = cyclesleft Mod 2
+        If m > 0 Then
+            Label1.ForeColor = Color.Black
+        Else
+            Label1.ForeColor = Color.Red
+        End If
+        Label1.Text = "STARTING IN " & cyclesleft.ToString & " SECONDS"
+        If cyclesleft > 0 Then
+            Timer1.Start()
+        Else
+            Try
+                If jobPath <> vbNullString Then
+                    Dim job As New Process
+                    job.StartInfo.FileName = jobPath
+                    job.Start()
+                    Select Case priority
+                        Case 0
+                            job.PriorityClass = ProcessPriorityClass.High
+                        Case 1
+                            job.PriorityClass = ProcessPriorityClass.RealTime
+                        Case 2
+                            job.PriorityClass = ProcessPriorityClass.AboveNormal
+                        Case Else
+                            job.PriorityClass = ProcessPriorityClass.Normal
+                    End Select
+
+
+                End If
+            Catch ex As Exception
+                MessageBox.Show("No jobpath entered. " & jobPath & " >> " & vbCrLf & ex.Message)
+            End Try
+
+
+            Me.Close()
+
+        End If
+    End Sub
+End Class
